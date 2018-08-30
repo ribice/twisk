cf8548eeb8989c5609f9333e882dc642fb9a0f33
Emir Ribic <ribice@gmail.com>
2018-08-30 06:41:03 +0200

    Remove unused struct

--- mock/model.go
+++ mock/model.go
@@ -2,15 +2,9 @@ package mock
 
 import (
 	"errors"
-
-	"github.com/go-pg/pg"
 )
 
 var (
 	// ErrGeneric used for testing purposes
 	ErrGeneric = errors.New("generic error used for testing purposes")
 )
-
-type bleja struct {
-	p *pg.DB
-}
